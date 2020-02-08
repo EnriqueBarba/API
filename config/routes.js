4606b9ed88e062a1d974dbcb79bbc6334e31abc8
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-02-08 11:34:00 +0100

    Cloudinary config

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('./cloudinary.config');
 //const authMiddleware = require('../middlewares/auth.middleware')
-//const upload = require('./cloudinary.config');
 
 module.exports = router;
