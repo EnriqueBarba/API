123a5c4a0123bf500d1a43242d210ed8872cc8d6
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-03-07 13:44:18 +0100

    TEST_heroku3

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,16 +1,9 @@
 const cors = require('cors')
 
-const allowedOrigins = [process.env.CORS_ORIGIN, 'http://localhost:3000']
+const origin = process.env.CORS_ORIGIN
 
 const corsMiddleware = cors({
-  origin: (origin, next) => {
-    const isAllowed = !origin || allowedOrigins.some(o => o === origin);
-    if(isAllowed) {
-      next(null,isAllowed);
-    } else {
-      next(createError(401, "Not allowed by CORS"))
-    }
-  },
+  origin: origin,
   credentials: true
 })
 
