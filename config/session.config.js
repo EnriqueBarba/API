5381e8f298046be03a4bfc5b3d228ce655e130fd
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-03-08 00:14:43 +0100

    Change

--- config/session.config.js
+++ config/session.config.js
@@ -3,12 +3,12 @@ const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7;
 
-module.exports = session({
+module.exports = console.info('Connection ', mongoose.connection) || session({
   secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
-  resave: true,
+  resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: true,
     httpOnly: true,
     maxAge: SESSION_MAX_AGE_SECONDS * 1000
   },
