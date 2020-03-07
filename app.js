01eb58bc17a9221712bd69b79a7a35ed16f239ae
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-03-07 20:43:00 +0100

    fixing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session);
 app.use((req, _, next) => {
-  console.log('REQUEST ', req.session)
+  console.log('REQUEST ', req)
   req.currentUser = req.session.user
   next()
 })
